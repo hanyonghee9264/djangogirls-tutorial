4d969f2641eebc80e1e3b20940819fc7c208518f
hanyonghee <hanyonghee9264@gmail.com>
2018-06-04 12:46:18 +0900

    HTML 파일을 불러올 때 render_to_string사용
    TEMPLATES_DIR를 지정 후, 해당 경로를 settings모듈의 TEMPLATES['DIRS'] 에
    추가
    render_to_string에서 템플릿 파일을 찾기 위해 주어진 경로는 settings모듈의 TEMPLATES['DIRS']에 있는
    폴더 목록을 기준으로 검색한다

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,45 +1,52 @@
 import os
 
 from django.http import HttpResponse
-
+from django.template.loader import render_to_string
 
 def post_list(request):
-    """
-    first/
-        first_file.txt
-        second/
-            second_file.txt
-            third/
-                module.py
-                fourth/
-                    fourth_file.txt
-
-    module.py에서
-    0. 현재 경로
-        os.path.abspath(__file__)
-
-    1. third/ 폴더의 경로
-        os.path.dirname(<현재경로>)
-
-    1-1. second/ 폴더의 경로
-        os.path.dirname(<third폴더의 경로>)
-
-    2. second/second_file.txt의 경로
-        os.path.join(<second폴더의 경로>, 'second_file.txt')
-
-    3. fourth/ 폴더의 경로
-        os.path.join(<현재경로>, 'fourth')
-
-    4. fourth/fourth_file.txt의 경로
-        os.path.join(<현재경로>, 'fourth', 'fourth_file.txt')
-    :param request:
-    :return:
-    """
-    cur_file_path = os.path.abspath(__file__)
-    blog_dir_path = os.path.dirname(cur_file_path)
-    app_dir_path = os.path.dirname(blog_dir_path)
-    templates_dir_path = os.path.join(app_dir_path, 'templates')
-    blog_template_file_path = os.path.join(templates_dir_path, 'blog', 'post_list.html')
-    print(blog_template_file_path)
-    html = open(blog_template_file_path, 'rt').read()
+    """..."""
+    # 경로에 해당하는 HTML파일을 문자열로 로드해줌
+    html = render_to_string('blog/post_list.html')
+    # 가져온 문자열을 돌려주기
     return HttpResponse(html)
+
+# def post_list(request):
+#     """
+#     first/
+#         first_file.txt
+#         second/
+#             second_file.txt
+#             third/
+#                 module.py
+#                 fourth/
+#                     fourth_file.txt
+#
+#     module.py에서
+#     0. 현재 경로
+#         os.path.abspath(__file__)
+#
+#     1. third/ 폴더의 경로
+#         os.path.dirname(<현재경로>)
+#
+#     1-1. second/ 폴더의 경로
+#         os.path.dirname(<third폴더의 경로>)
+#
+#     2. second/second_file.txt의 경로
+#         os.path.join(<second폴더의 경로>, 'second_file.txt')
+#
+#     3. fourth/ 폴더의 경로
+#         os.path.join(<현재경로>, 'fourth')
+#
+#     4. fourth/fourth_file.txt의 경로
+#         os.path.join(<현재경로>, 'fourth', 'fourth_file.txt')
+#     :param request:
+#     :return:
+#     """
+    # cur_file_path = os.path.abspath(__file__)
+    # blog_dir_path = os.path.dirname(cur_file_path)
+    # app_dir_path = os.path.dirname(blog_dir_path)
+    # templates_dir_path = os.path.join(app_dir_path, 'templates')
+    # blog_template_file_path = os.path.join(templates_dir_path, 'blog', 'post_list.html')
+    # print(blog_template_file_path)
+    # html = open(blog_template_file_path, 'rt').read()
+    # return HttpResponse(html)
